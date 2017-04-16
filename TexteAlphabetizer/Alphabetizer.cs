3fee228f0ae14115643a58ae8223e26f730ef429
trixiecatsrule <confirmationemailconfirmer@gmail.com>
2017-04-15 17:33:32 -0700

    Adding all the files.

---
+++ TexteAlphabetizer/Alphabetizer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TexteAlphabetizer
+{
+    class Alphabetizer : IComparer
+    {
+
+        private Dictionary<char, int> alphabetIndex = new Dictionary<char, int>()
+        {
+            { ' ', -100 }, //should sort to the front
+            { 'a', 0 },
+            { 'ā', 1 },
+            { 'ω', 2 },
+            { 'd', 3 },
+            { 'э', 4 },
+            { 'и', 5 },
+            { 'e', 6 },
+            { 'ē', 7 },
+            { 'λ', 8 },
+            { 'f', 9 },
+            { 'g', 10 },
+            { 'ж', 11 },
+            { 'л', 12 },
+            { 'h', 13 },
+            { 'c', 14 },
+            { 'i', 15 },
+            { 'ī', 16 },
+            { 'j', 17 },
+            { 'k', 18 },
+            { 'l', 19 },
+            { 'm', 20 },
+            { 'n', 21 },
+            { 'ц', 22 },
+            { 'o', 23 },
+            { 'ч', 24 },
+            { 'з', 25 },
+            { 'ø', 26 },
+            { 'p', 27 },
+            { 'r', 28 },
+            { 's', 29 },
+            { 'x', 30 },
+            { 't', 31 },
+            { 'q', 32 },
+            { 'u', 33 },
+            { 'ū', 34 },
+            { 'π', 35 },
+            { 'w', 36 },
+            { 'я', 37 },
+            { 'v', 38 },
+            { 'b', 39 },
+            { 'ł', 40 },
+            { 'z', 41 },
+            { 'ψ', 42 },
+            { 'г', 43 },
+            { '-', 100 } //should sort to the end
+        };
+
+        /**
+         * Returns -1 if string a is earlier in the alphabet than string b. Ie: true corresponds to a being earlier, false to b
+         * @param a The first string
+         * @param b The second string
+         * @return If a is before b, returns -1 | If a is less than b, returns 1 | If they're equal, returns 0
+         */
+        int IComparer.Compare(object x, object y)
+        {
+            string a = (string)x;
+            string b = (string)y;
+
+            int aSpace = a.IndexOf('.');
+            int bSpace = b.IndexOf('.');
+
+            a = a.Substring(0, aSpace).ToLower();
+            b = b.Substring(0, bSpace).ToLower();
+
+            string shorter; //The shorter of the two strings.
+            if (a.Length < b.Length) { shorter = a; }
+            else { shorter = b; }
+
+            for (int i = 0; i < shorter.Length; i++)
+            {
+                char aChar = a[i];
+                char bChar = b[i];
+
+                int aIndex = alphabetIndex[aChar];
+                int bIndex = alphabetIndex[bChar];
+
+                if (aIndex < bIndex) //a is before b
+                {
+                    return -1;
+                }
+                else if (bIndex < aIndex) //b is before a
+                {
+                    return 1;
+                }
+                //Nothing returns here if the characters are the same. On to the next one!
+            }
+
+            //If we get here, one string begins the other (eg: "car" and "cars") or they're the same
+            if (a.Equals(b)) { return 0; }
+
+            if (shorter.Equals(a)) { return -1; } //a is the shorter and therefore sequentially earlier; return true
+            else { return 1; } //b is the shorter and therefore sequentially earlier; return false
+        }
+    }
+}
---
+++ TexteAlphabetizer/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TexteAlphabetizer
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.AcceptsReturn = true;
+            this.input.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.input.Location = new System.Drawing.Point(13, 13);
+            this.input.MaxLength = 60000;
+            this.input.Multiline = true;
+            this.input.Name = "input";
+            this.input.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.input.Size = new System.Drawing.Size(619, 346);
+            this.input.TabIndex = 0;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(285, 365);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Alphabetize";
+            this.goButton.UseVisualStyleBackColor = true;
+            this.goButton.Click += new System.EventHandler(this.goButton_Click);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 396);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.input);
+            this.Name = "Form";
+            this.Text = "Alphabetizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button goButton;
+
+        void buttonClick()
+        {
+            string output = "";
+
+            ArrayList toSort = new ArrayList();
+
+            string[] lines = input.Text.Split("\r\n".ToCharArray());
+
+            foreach (string line in lines)
+            {
+                if (line != "")
+                {
+                    toSort.Add(line);
+                }
+            }
+
+            toSort = sortDictionary(toSort);
+
+            foreach (string entry in toSort)
+            {
+                output += entry + "\r\n";
+            }
+
+            input.Text = output.Substring(0, output.Length - "\r\n".Length);
+
+            //Debug.WriteLine(alphabetize("Яāwłāиz", "Dłøяиwnaklчи")); //Should return false
+            //Debug.WriteLine(alphabetize("Ānawreftīиānωāktē", "Цзиωeиrī")); //Should return true
+            //Debug.WriteLine(alphabetize("Fg", "fga")); //Should return true
+        }
+
+        /**
+         * Sorts the dictionary by textē's alphabet.
+         * @param dictionary The dictionary, already alphabetized.
+         * @return The dictionary, with the word added.
+         */
+        ArrayList sortDictionary(ArrayList dictionary)
+        {
+            IComparer alphabetizer = new Alphabetizer();
+            dictionary.Sort(alphabetizer);
+            return dictionary;
+        }
+    }
+}
+
